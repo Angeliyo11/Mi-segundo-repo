bcceb2689456823d856cf5b504f5bc620f41e696
angeliyo <angelonce60@gmail.com>
2023-11-21 22:00:56 -0500

    Mi segundo commit

---
+++ Ejercicios 1.py
@@ -0,0 +1,13 @@
+a="Bienvenidos a mi 'Primer' Programa"
+b='En mi lenguaje de Programacion "Python"'
+
+print(a+" "+b)
+b="con el uso de Python"
+
+print(a,b,"Gracias")
+
+a=3 
+b=5
+c=7
+
+print(f"El numero {a} es el primer valor impar, el segundo es {b} y el tecercero es {c}")
